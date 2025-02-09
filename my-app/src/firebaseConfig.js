5438d89f814621513efdbfc8d28abd9a4a065da9
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-02-09 16:01:59 +0100

    add

--- my-app/src/firebaseConfig.js
+++ my-app/src/firebaseConfig.js
@@ -1,22 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut,
-} from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "TWÓJ_API_KEY",
-  authDomain: "TWÓJ_AUTH_DOMAIN",
-  projectId: "TWÓJ_PROJECT_ID",
-  storageBucket: "TWÓJ_STORAGE_BUCKET",
-  messagingSenderId: "TWÓJ_MESSAGING_SENDER_ID",
-  appId: "TWÓJ_APP_ID",
+  apiKey: "AIzaSyAvZGjY960ozg33gdfmzwf8sTcmCq3do6U",
+  authDomain: "kapusta-c9147.firebaseapp.com",
+  projectId: "kapusta-c9147",
+  storageBucket: "kapusta-c9147.firebasestorage.app",
+  messagingSenderId: "91415513983",
+  appId: "1:91415513983:web:a3316404cabbf66496684a",
+  measurementId: "G-CNFFWNL7TF",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export { auth, provider, signInWithPopup, signOut };
+const analytics = getAnalytics(app);
